d69aa5ec89a8a12adb5fc6c94066b8b6660baea0
muhammadsuheer <58456635+muhammadsuheer@users.noreply.github.com>
2020-02-21 21:09:34 +0500

    Updates

---
+++ precache-manifest.81bf064bcfb663cd45f1e35350b582e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a95167e932e408f630c68d3bc5fe1a72",
+    "url": "/robo/index.html"
+  },
+  {
+    "revision": "5cd5d763204349fe975b",
+    "url": "/robo/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "5383d299cd867e1fee0a",
+    "url": "/robo/static/css/main.17b86e0e.chunk.css"
+  },
+  {
+    "revision": "5cd5d763204349fe975b",
+    "url": "/robo/static/js/2.211408e1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robo/static/js/2.211408e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5383d299cd867e1fee0a",
+    "url": "/robo/static/js/main.3b4ea78b.chunk.js"
+  },
+  {
+    "revision": "9b8eb86b7be7fd3e1625",
+    "url": "/robo/static/js/runtime-main.d42d5b2f.js"
+  }
+]);
